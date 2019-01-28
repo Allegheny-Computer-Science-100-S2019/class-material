a6adac52090dbb09ea525ac32f47f0e38ea3fb48
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-01-28 15:56:15 -0500

    Completed class program Jan 28

--- src/instructor/VolumeSurfaceArea.java
+++ src/instructor/VolumeSurfaceArea.java
@@ -9,6 +9,17 @@ public class VolumeSurfaceArea {
   * the neccessary calculations.
   */
   public static void main (String [] args) {
+    // variable declarations
+    int radius;
+    double area;
+    double pi;
 
+    // initialize variables
+    pi = 3.14;
+    radius = 10;
+
+    // expressions
+    area = 3 * pi * radius * radius;
+    System.out.println("Surface Area of the Sphere: " + area);
   }
 }
