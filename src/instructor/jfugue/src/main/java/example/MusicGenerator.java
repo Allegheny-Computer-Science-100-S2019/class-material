3b839cffa472fd98975887e09ee23ff66b4adc11
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-02-20 10:45:25 -0500

    jfugue example files

---
+++ src/instructor/jfugue/src/main/java/example/MusicGenerator.java
@@ -0,0 +1,23 @@
+package example;
+
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Date;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MusicGenerator {
+
+  public static void main(String[] args) {
+
+    // TODO: create your own music strings
+    //String part1 = "C5q C5q C5i. D5s E5q E5i. D5s E5i. F5s G5h";
+    //String part2 = "C6i C6i C6i G5i G5i G5i E5i E5i E5i C5i";
+    //String part3 = "C5i C5i";
+    //String part4 = "G5i. F5s E5i. D5s C5h";
+
+  }
+}
