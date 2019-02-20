75d8971641aa0a536b4a1dffaf01c52d7f4fba4b
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-02-20 11:51:48 -0500

    Completed class program from Feb 20

--- src/instructor/Payments.java
+++ src/instructor/Payments.java
@@ -6,10 +6,10 @@ public class Payments {
 		double loanAmount;
 		double rate;
 		int loanTerm;
-		
+
 		// create an instance of the Scanner
 		Scanner scan = new Scanner (System.in);
-		
+
 		// obtain user's input
 		System.out.println("Enter a loan amount");
 		loanAmount = scan.nextDouble();
@@ -17,6 +17,12 @@ public class Payments {
 		rate = scan.nextDouble();
 		System.out.println("Enter loan term");
 		loanTerm = scan.nextInt();
-		
-	}	
+
+		double term = Math.pow(1 + rate, loanTerm);
+		System.out.println("Term is: " + term);
+
+		double payment = loanAmount * rate * (term / (term - 1));
+		System.out.println(" Payment: " + payment);
+
+	}
 }
--- src/instructor/jfugue/src/main/java/example/MusicGenerator.java
+++ src/instructor/jfugue/src/main/java/example/MusicGenerator.java
@@ -13,11 +13,38 @@ public class MusicGenerator {
 
   public static void main(String[] args) {
 
-    // TODO: create your own music strings
-    //String part1 = "C5q C5q C5i. D5s E5q E5i. D5s E5i. F5s G5h";
-    //String part2 = "C6i C6i C6i G5i G5i G5i E5i E5i E5i C5i";
-    //String part3 = "C5i C5i";
-    //String part4 = "G5i. F5s E5i. D5s C5h";
+    File inputFile = null;
+    Scanner scanner = null;
 
+    try {
+      inputFile = new File("input/user_inputs.txt");
+      scanner = new Scanner(inputFile);
+    } catch (FileNotFoundException noFile) {
+      System.out.println("Unable to find the file");
+    }
+
+    String instrument = scanner.next();
+    String tempo = scanner.next();
+
+    String choice = "I[" + instrument + "] T[" + tempo + "]";
+
+    // TODO: create your own music strings
+    String part1 = "C5q C5q C5i. D5s E5q E5i. D5s E5i. F5s G5h";
+    String part2 = "C6i C6i C6i G5i G5i G5i E5i E5i E5i C5i";
+    String part3 = "C5i C5i";
+    String part4 = "G5i. F5s E5i. D5s C5h";
+
+    //String music = "A B C";
+
+    Pattern piece = new Pattern();
+    piece.add(choice);
+    piece.add(part1);
+    piece.add(part2).add(part3);
+    piece.add(part4);
+
+    piece.repeat(2);
+
+    Player player = new Player();
+    player.play(piece);
   }
 }
